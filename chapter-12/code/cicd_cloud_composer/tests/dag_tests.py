31c3946804f3b962581bfad14b947c9428110132
adiwijaya <adiwijaya25@gmail.com>
2024-02-10 11:46:09 +0800

    Finalize Chapter 12

--- chapter-12/code/cicd_cloud_composer/tests/dag_tests.py
+++ chapter-12/code/cicd_cloud_composer/tests/dag_tests.py
@@ -1,9 +1,24 @@
+# Copyright 2023 Google LLC
+
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+
+#     https://www.apache.org/licenses/LICENSE-2.0
+
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 """
 DAG Unit Tests
 """
 
 import unittest
 from airflow.models import DagBag
+
 
 class TestDags(unittest.TestCase):
     """DAG Test Case"""
@@ -20,9 +35,10 @@ class TestDags(unittest.TestCase):
             print(dag)
 
         self.assertEqual(
-                len(self.dagbag.import_errors), 0,
-                "DAG Errors: {}".format(self.dagbag.import_errors)
-            )
+            len(self.dagbag.import_errors), 0,
+            "DAG Errors: {}".format(self.dagbag.import_errors)
+        )
+
 
 if __name__ == '__main__':
     SUITE = unittest.TestLoader().loadTestsFromTestCase(TestDags)
